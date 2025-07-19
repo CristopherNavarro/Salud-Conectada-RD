33985b946a1b5e066f640057e7facaa4785c36d6
Cristopher Navarro <cristophernavarro14092006@gmail.com>
2025-07-19 04:19:53 +0000

    Arreglo + imagenes nuevas

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -17,19 +17,19 @@ const ImageCarousel = () => {
       className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
     >
       <div>
-        <Image src="/image1.jpg" alt="Image 1" width={600} height={400} className="w-full h-full object-cover" />
+        <Image src="/image1.jpg" alt="Image 1" width={1920} height={1080} className="w-full h-full object-cover" priority />
       </div>
       <div>
-        <Image src="/image2.jpg" alt="Image 2" width={600} height={400} className="w-full h-full object-cover" />
+        <Image src="/image2.jpg" alt="Image 2" width={1920} height={1080} className="w-full h-full object-cover" />
       </div>
       <div>
-        <Image src="/image3.jpg" alt="Image 3" width={600} height={400} className="w-full h-full object-cover" />
+        <Image src="/image3.jpg" alt="Image 3" width={1920} height={1080} className="w-full h-full object-cover" />
       </div>
       <div>
-        <Image src="/image4.jpg" alt="Image 4" width={600} height={400} className="w-full h-full object-cover" />
+        <Image src="/image4.jpg" alt="Image 4" width={1920} height={1080} className="w-full h-full object-cover" />
       </div>
       <div>
-        <Image src="/image5.jpg" alt="Image 5" width={600} height={400} className="w-full h-full object-cover" />
+        <Image src="/image5.jpg" alt="Image 5" width={1920} height={1080} className="w-full h-full object-cover" />
       </div>
  </Carousel>
  );
