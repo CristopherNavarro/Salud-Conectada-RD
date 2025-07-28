47bc938f26930be71e918a4b38aa86a553eadec4
Cristopher Navarro <cristophernavarro14092006@gmail.com>
2025-07-28 23:43:44 +0000

    Puesto el chat y el webhook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import Chatbot from "@/components/Chatbot";
 
 const ptSans = PT_Sans({
   subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
         </div>
         <Toaster />
         
+        <Chatbot />
       </body>
     </html>
   );
