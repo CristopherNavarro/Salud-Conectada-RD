f18b7faf148b57304d28a8ee9dfbaa01c19fbd0f
Cristopher Navarro <cristophernavarro14092006@gmail.com>
2025-07-29 02:11:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,8 @@ import {googleAI} from '@genkit-ai/googleai';
 import {mistral} from 'genkitx-mistral';
 
 export const ai = genkit({
-  plugins: [googleAI(), mistral({apiKey: 'LwEJv7Yfhk8yO29XH8DamRjvHS4RwnLz'})],
+  plugins: [
+    googleAI(),
+    mistral({apiKey: 'LwEJv7Yfhk8yO29XH8DamRjvHS4RwnLz'}),
+  ],
 });
